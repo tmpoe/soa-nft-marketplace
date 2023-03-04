98dbae1e8a80172256d89c4d97cfe2edfb0fafad
tmpITK <torok.mark.patrik@gmail.com>
2023-03-04 10:02:19 +0100

    Add first basic test

---
+++ test/Nft.ts
@@ -0,0 +1,14 @@
+import { assert, expect } from "chai"
+import { ethers } from "hardhat"
+
+describe("Nft contract tests", () => {
+    it("is mintable", async () => {
+        const [owner] = await ethers.getSigners()
+
+        const Nft = await ethers.getContractFactory("Nft")
+        const hardhatNft = await Nft.deploy()
+        await hardhatNft.deployed()
+
+        await expect(hardhatNft.mint()).to.emit(hardhatNft, "NftMinted").withArgs(owner)
+    })
+})
