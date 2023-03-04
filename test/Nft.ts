c508004b3089a3c548c029a25d70d70d273127cc
tmpITK <torok.mark.patrik@gmail.com>
2023-03-04 11:06:55 +0100

    Add owner transfer test

--- test/Nft.ts
+++ test/Nft.ts
@@ -29,3 +29,23 @@ describe("Nft minting tests", () => {
             .withArgs(addr1.address)
     })
 })
+
+describe("Nft behaviour tests", () => {
+    let owner: SignerWithAddress, addr1: SignerWithAddress, nft, hardhatNft: Nft
+    const firstIndex = 0
+    const secondIndex = 1
+
+    beforeEach(async () => {
+        ;[owner, addr1] = await ethers.getSigners()
+
+        nft = await ethers.getContractFactory("Nft")
+        hardhatNft = await nft.deploy()
+        await hardhatNft.deployed()
+        await hardhatNft.mint()
+    })
+
+    it("can change owners", async () => {
+        await hardhatNft.transferFrom(owner.address, addr1.address, 0)
+        assert(addr1.address === (await hardhatNft.ownerOf(0)))
+    })
+})
