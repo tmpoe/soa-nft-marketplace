36ed082e410c7552c42a6de62d23d081f14ba1bb
tmpITK <torok.mark.patrik@gmail.com>
2023-03-05 12:25:42 +0100

    Make randomized nft with minimal test

--- contracts/Nft.sol
+++ contracts/Nft.sol
@@ -2,29 +2,74 @@
 pragma solidity ^0.8.9;
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@chainlink/contracts/src/v0.8/VRFConsumerBaseV2.sol";
+import "@chainlink/contracts/src/v0.8/interfaces/VRFCoordinatorV2Interface.sol";
 
 error Nft__InsufficientFunds();
 
-contract Nft is ERC721 {
+contract Nft is ERC721, VRFConsumerBaseV2 {
+    enum Breed {
+        Ragdoll,
+        Sphynx,
+        Persian
+    }
+
     uint256 immutable i_mintingFee;
-    event NftMinted(address owner);
+
+    VRFCoordinatorV2Interface private immutable i_vrfCoordinator;
+    uint64 private immutable i_subscriptionId;
+    bytes32 private immutable i_gasLane;
+    uint32 private immutable i_callbackGasLimit;
+    uint16 private constant REQUEST_CONFIRMATIONS = 3;
+    uint32 private constant NUM_WORDS = 1;
+
+    mapping(uint256 => address) public s_requestIdToSender;
+
+    event NftRequested(uint256 requestId, address requester);
+    event NftMinted(address owner, Breed breed);
 
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    constructor(uint256 mintingFee) ERC721("MyToken", "MTK") {
+    constructor(
+        uint256 mintingFee,
+        address vrfCoordinatorV2,
+        uint64 subscriptionId,
+        bytes32 gasLane,
+        uint32 callbackGasLimit
+    ) ERC721("MyToken", "MTK") VRFConsumerBaseV2(vrfCoordinatorV2) {
         i_mintingFee = mintingFee;
+        i_subscriptionId = subscriptionId;
+        i_vrfCoordinator = VRFCoordinatorV2Interface(vrfCoordinatorV2);
+        i_gasLane = gasLane;
+        i_callbackGasLimit = callbackGasLimit;
     }
 
-    function mint() public payable {
+    function requestNft() public payable returns (uint256 requestId) {
         if (msg.value < i_mintingFee) {
             revert Nft__InsufficientFunds();
         }
+        requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            NUM_WORDS
+        );
+        s_requestIdToSender[requestId] = msg.sender;
+        emit NftRequested(requestId, msg.sender);
+    }
 
+    function fulfillRandomWords(uint256 requestId, uint256[] memory randomWords)
+        internal
+        override
+    {
+        address owner = s_requestIdToSender[requestId]; // should I delete after query?
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
-        _safeMint(msg.sender, tokenId);
-        emit NftMinted(msg.sender);
+        uint256 breed = randomWords[0] % 3;
+        _safeMint(owner, tokenId);
+        emit NftMinted(owner, Breed(breed));
     }
 
     function getMintingFee() public view returns (uint256) {
--- test/Nft.ts
+++ test/Nft.ts
@@ -1,11 +1,23 @@
 import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers"
 import { assert, expect } from "chai"
 import { ethers } from "hardhat"
-import { Nft } from "../typechain-types"
-import { BigNumber } from "ethers"
+import { Nft, VRFCoordinatorV2Mock } from "../typechain-types"
+import { BigNumber, ContractReceipt } from "ethers"
+
+const BASE_FEE = "250000000000000000" // 0.25 is this the premium in LINK?
+const GAS_PRICE_LINK = 1e9 // link per gas, is this the gas lane? // 0.000000001 LINK per gas
+const FUND_AMOUNT = "1000000000000000000000"
+const CALLBACK_GAS_LIMIT = "500000"
 
 describe("Nft minting tests", () => {
-    let owner: SignerWithAddress, addr1: SignerWithAddress, nft, hardhatNft: Nft
+    let owner: SignerWithAddress,
+        addr1: SignerWithAddress,
+        subscriptionId: number,
+        nft,
+        hardhatNft: Nft,
+        vrfCoordinatorV2Mock,
+        hardhatVrfCoordinatorV2Mock: VRFCoordinatorV2Mock
+
     const firstIndex = 0
     const secondIndex = 1
 
@@ -13,31 +25,52 @@ describe("Nft minting tests", () => {
         ;[owner, addr1] = await ethers.getSigners()
 
         nft = await ethers.getContractFactory("Nft")
-        hardhatNft = await nft.deploy(1)
+        vrfCoordinatorV2Mock = await ethers.getContractFactory("VRFCoordinatorV2Mock")
+        hardhatVrfCoordinatorV2Mock = await vrfCoordinatorV2Mock.deploy(BASE_FEE, GAS_PRICE_LINK)
+
+        const response = await hardhatVrfCoordinatorV2Mock.createSubscription({
+            from: owner.address,
+        })
+
+        const receipt: ContractReceipt = await response.wait()
+        subscriptionId = receipt.events![0].args!.subId
+
+        await hardhatVrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT, {
+            from: owner.address,
+        })
+        const gasLane = ethers.constants.HashZero
+        hardhatNft = await nft.deploy(
+            1,
+            hardhatVrfCoordinatorV2Mock.address,
+            subscriptionId,
+            gasLane,
+            CALLBACK_GAS_LIMIT
+        )
         await hardhatNft.deployed()
+
+        await hardhatVrfCoordinatorV2Mock.addConsumer(subscriptionId, hardhatNft.address)
     })
 
-    it("is mintable", async () => {
+    it("is requestable to mint", async () => {
         const fee: BigNumber = await hardhatNft.getMintingFee()
-        await expect(hardhatNft.mint({ value: fee.toString() }))
-            .to.emit(hardhatNft, "NftMinted")
-            .withArgs(owner.address)
-        const nftOwner = await hardhatNft.ownerOf(0)
-        assert(nftOwner === owner.address)
+
+        await expect(hardhatNft.requestNft({ value: fee.toString(), from: owner.address }))
+            .to.emit(hardhatNft, "NftRequested")
+            .withArgs(1, owner.address)
     })
 
-    it("allows anyone to mint", async () => {
+    it.skip("allows anyone to request", async () => {
         const fee: BigNumber = await hardhatNft.getMintingFee()
-        await expect(hardhatNft.mint({ value: fee.toString() }))
-            .to.emit(hardhatNft, "NftMinted")
+        await expect(hardhatNft.requestNft({ value: fee.toString() }))
+            .to.emit(hardhatNft, "NftRequested")
             .withArgs(owner.address)
-        await expect(hardhatNft.connect(addr1).mint({ value: fee.toString() }))
-            .to.emit(hardhatNft, "NftMinted")
+        await expect(hardhatNft.connect(addr1).requestNft({ value: fee.toString() }))
+            .to.emit(hardhatNft, "NftRequested")
             .withArgs(addr1.address)
     })
 
-    it("fails to mint in case of insufficient funds", async () => {
-        await expect(hardhatNft.mint()).to.be.revertedWithCustomError(
+    it.skip("fails to mint in case of insufficient funds", async () => {
+        await expect(hardhatNft.requestNft()).to.be.revertedWithCustomError(
             hardhatNft,
             "Nft__InsufficientFunds"
         )
@@ -45,7 +78,13 @@ describe("Nft minting tests", () => {
 })
 
 describe("Nft behaviour tests", () => {
-    let owner: SignerWithAddress, addr1: SignerWithAddress, nft, hardhatNft: Nft
+    let owner: SignerWithAddress,
+        addr1: SignerWithAddress,
+        subscriptionId: number,
+        nft,
+        hardhatNft: Nft,
+        vrfCoordinatorV2Mock,
+        hardhatVrfCoordinatorV2Mock: VRFCoordinatorV2Mock
     const firstIndex = 0
     const secondIndex = 1
 
@@ -53,14 +92,38 @@ describe("Nft behaviour tests", () => {
         ;[owner, addr1] = await ethers.getSigners()
 
         nft = await ethers.getContractFactory("Nft")
-        hardhatNft = await nft.deploy(1)
+
+        vrfCoordinatorV2Mock = await ethers.getContractFactory("VRFCoordinatorV2Mock")
+        hardhatVrfCoordinatorV2Mock = await vrfCoordinatorV2Mock.deploy(BASE_FEE, GAS_PRICE_LINK)
+
+        const response = await hardhatVrfCoordinatorV2Mock.createSubscription()
+        const receipt: ContractReceipt = await response.wait()
+        subscriptionId = receipt.events![0].args!.subId
+
+        await hardhatVrfCoordinatorV2Mock.fundSubscription(subscriptionId, FUND_AMOUNT)
+        const gasLane = "0xd89b2bf150e3b9e13446986e571fb9cab24b13cea0a43ea20a6049a85cc807cc",
+            hardhatNft = await nft.deploy(
+                1,
+                hardhatVrfCoordinatorV2Mock.address,
+                subscriptionId,
+                gasLane,
+                CALLBACK_GAS_LIMIT
+            )
+        await hardhatNft.deployed()
+
         await hardhatNft.deployed()
         const fee: BigNumber = await hardhatNft.getMintingFee()
-        await hardhatNft.mint({ value: fee.toString() })
+        await hardhatNft.requestNft({ value: fee.toString() })
     })
 
-    it("can change owners", async () => {
+    it.skip("can change owners", async () => {
         await hardhatNft.transferFrom(owner.address, addr1.address, 0)
         assert(addr1.address === (await hardhatNft.ownerOf(0)))
+    })
+
+    it.skip("does not allow other then the owner to initiate owner change", async () => {
+        await expect(
+            hardhatNft.connect(addr1).transferFrom(owner.address, addr1.address, 0)
+        ).to.be.revertedWith("ERC721: transfer caller is not owner nor approved")
     })
 })
