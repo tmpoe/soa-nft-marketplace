45a948b09d4b282f2bbf49179ac30fa5affe1ab9
tmpITK <torok.mark.patrik@gmail.com>
2023-03-11 09:53:20 +0100

    Add env file with vars

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,19 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import { HardhatUserConfig } from "hardhat/config"
+import "@nomicfoundation/hardhat-toolbox"
+import "@nomiclabs/hardhat-etherscan"
+import "hardhat-deploy"
+import "solidity-coverage"
+import "dotenv"
+
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY
+// optional
+const MNEMONIC = process.env.MNEMONIC
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
-};
+    solidity: "0.8.17",
+}
 
-export default config;
+export default config
