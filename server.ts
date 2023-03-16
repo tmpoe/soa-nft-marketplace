12c0a43c6b16307a747bc229ac99606ab6d754ee
tmpITK <torok.mark.patrik@gmail.com>
2023-03-16 17:35:21 +0100

    Freeze the API idea for now

--- server.ts
+++ server.ts
@@ -6,6 +6,10 @@ dotenv.config()
 const app: Express = express()
 const port = process.env.SERVER_PORT
 
+// will not use this for now, first will make the frontend work,
+// and will move smart contract calls behind an API call - RN this
+// IDK how this could work and would rather make something that works
+// first
 app.get("/", (req: Request, res: Response) => {
     res.send("Express + TypeScript Server")
 })
