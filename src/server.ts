26e822864efc361af8b2fffc5f60691ded408f34
tmpITK <torok.mark.patrik@gmail.com>
2023-04-08 08:06:27 +0200

    Revert "Add minimal server error handling"
    This reverts commit cef0dd3aee8d59d83839cc9bcf893ebc67585627.

--- src/server.ts
+++ src/server.ts
@@ -4,13 +4,8 @@ import mintNft from "./mint_nft"
 const app = express()
 
 app.post("/:address", async (req, res) => {
-    try {
-        await mintNft(req.params.address)
-        res.send(`nft requested for ${req.params.address}`)
-    } catch (e) {
-        res.status(500)
-        res.send(e)
-    }
+    await mintNft(req.params.address)
+    res.send(`nft requested for ${req.params.address}`)
 })
 
 app.listen(5000, () => {
