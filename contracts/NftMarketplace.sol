3c8e1af04b6351a3cacae2e9af3f7b81cfdbc47e
tmpITK <torok.mark.patrik@gmail.com>
2023-05-06 08:49:17 +0200

    Update contract to not use nftId but tokenId

--- contracts/NftMarketplace.sol
+++ contracts/NftMarketplace.sol
@@ -9,8 +9,8 @@ error NftMarketplace__InsufficientFunds();
 error NftMarketplace__IncorrectPrice();
 error NftMarketplace__Unauthorized();
 error NftMarketplace__NoPriceSetForListing();
-error NftMarketplace__ItemAlreadyListed(uint256 nftId, address nftAddress);
-error NftMarketplace__ItemNotListed(uint256 nftId, address nftAddress);
+error NftMarketplace__ItemAlreadyListed(uint256 tokenId, address nftAddress);
+error NftMarketplace__ItemNotListed(uint256 tokenId, address nftAddress);
 error NftMarketplace__NotApprovedForMarketplace();
 error NftMarketplace__SellerCannotBeBuyer();
 error NftMarketplace__NoProceedingsToWithdraw(address);
@@ -33,16 +33,16 @@ contract NftMarketplace is Ownable, ReentrancyGuard {
     mapping(address => uint256) private s_proceedings;
 
     event NftRequested(address requester);
-    event NftMinted(address owner, uint256 tokenId);
-    event NftListed(uint256 nftId, address owner, uint256 price, address ierc721TokenAddress);
-    event NftSold(address owner, uint256 nftId, address ierc721TokenAddress, uint256 price);
+    event NftMinted(address owner, uint256 tokenId, address ierc721TokenAddress);
+    event NftListed(uint256 tokenId, address owner, uint256 price, address ierc721TokenAddress);
+    event NftSold(address owner, uint256 tokenId, address ierc721TokenAddress, uint256 price);
     event NftListingUpdated(
-        uint256 nftId,
+        uint256 tokenId,
         address owner,
         uint256 price,
         address ierc721TokenAddress
     );
-    event NftListingCancelled(uint256 nftId, address owner, address ierc721TokenAddress);
+    event NftListingCancelled(uint256 tokenId, address owner, address ierc721TokenAddress);
 
     constructor(address nftContractAddress, uint256 mintingFee) {
         nftContract = Nft(nftContractAddress);
@@ -58,16 +58,16 @@ contract NftMarketplace is Ownable, ReentrancyGuard {
 
     function mintNft(string memory ipfsHash, address owner) external onlyOwner {
         uint256 tokenId = nftContract.mint(owner, ipfsHash);
-        emit NftMinted(owner, tokenId);
+        emit NftMinted(owner, tokenId, address(nftContract));
     }
 
-    function buyNft(uint256 nftId, address ierc721TokenAddress)
+    function buyNft(uint256 tokenId, address ierc721TokenAddress)
         public
         payable
-        isListed(nftId, ierc721TokenAddress)
+        isListed(tokenId, ierc721TokenAddress)
         nonReentrant
     {
-        Listing memory listing = s_listings[ierc721TokenAddress][nftId];
+        Listing memory listing = s_listings[ierc721TokenAddress][tokenId];
         if (msg.value != listing.price) {
             revert NftMarketplace__IncorrectPrice();
         }
@@ -77,11 +77,11 @@ contract NftMarketplace is Ownable, ReentrancyGuard {
 
         ERC721 nft = ERC721(ierc721TokenAddress);
 
-        delete s_listings[ierc721TokenAddress][nftId];
+        delete s_listings[ierc721TokenAddress][tokenId];
         s_proceedings[listing.seller] += msg.value;
 
-        nft.safeTransferFrom(listing.seller, msg.sender, nftId);
-        emit NftSold(msg.sender, nftId, ierc721TokenAddress, msg.value);
+        nft.safeTransferFrom(listing.seller, msg.sender, tokenId);
+        emit NftSold(msg.sender, tokenId, ierc721TokenAddress, msg.value);
     }
 
     function withdrawProceedings() external {
@@ -97,74 +97,74 @@ contract NftMarketplace is Ownable, ReentrancyGuard {
     }
 
     function listNft(
-        uint256 nftId,
+        uint256 tokenId,
         address ierc721TokenAddress,
         uint256 price
-    ) public isOwner(nftId, ierc721TokenAddress) notListed(nftId, ierc721TokenAddress) {
+    ) public isOwner(tokenId, ierc721TokenAddress) notListed(tokenId, ierc721TokenAddress) {
         if (price <= 0) {
             revert NftMarketplace__NoPriceSetForListing();
         }
         IERC721 nft = IERC721(ierc721TokenAddress);
 
-        if (nft.getApproved(nftId) != address(this)) {
+        if (nft.getApproved(tokenId) != address(this)) {
             revert NftMarketplace__NotApprovedForMarketplace();
         }
 
-        s_listings[ierc721TokenAddress][nftId] = Listing(price, msg.sender);
-        emit NftListed(nftId, msg.sender, price, ierc721TokenAddress);
+        s_listings[ierc721TokenAddress][tokenId] = Listing(price, msg.sender);
+        emit NftListed(tokenId, msg.sender, price, ierc721TokenAddress);
     }
 
     function updateListing(
-        uint256 nftId,
+        uint256 tokenId,
         address ierc721TokenAddress,
         uint256 price
     )
         public
         payable
-        isOwner(nftId, ierc721TokenAddress)
-        isListed(nftId, ierc721TokenAddress)
+        isOwner(tokenId, ierc721TokenAddress)
+        isListed(tokenId, ierc721TokenAddress)
         nonReentrant
     {
         if (price <= 0) {
             revert NftMarketplace__NoPriceSetForListing();
         }
-        s_listings[ierc721TokenAddress][nftId].price = price;
-        emit NftListingUpdated(nftId, msg.sender, price, ierc721TokenAddress);
+        s_listings[ierc721TokenAddress][tokenId].price = price;
+        emit NftListingUpdated(tokenId, msg.sender, price, ierc721TokenAddress);
     }
 
-    function cancelListing(uint256 nftId, address ierc721TokenAddress)
+    function cancelListing(uint256 tokenId, address ierc721TokenAddress)
         public
-        isOwner(nftId, ierc721TokenAddress)
-        isListed(nftId, ierc721TokenAddress)
+        isOwner(tokenId, ierc721TokenAddress)
+        isListed(tokenId, ierc721TokenAddress)
     {
-        delete s_listings[ierc721TokenAddress][nftId];
-        emit NftListingCancelled(nftId, msg.sender, ierc721TokenAddress);
+        delete s_listings[ierc721TokenAddress][tokenId];
+        emit NftListingCancelled(tokenId, msg.sender, ierc721TokenAddress);
     }
 
     function getMintingFee() public view returns (uint256) {
         return i_mintingFee;
     }
 
-    modifier isOwner(uint256 nftId, address ierc721TokenAddress) {
+    modifier isOwner(uint256 tokenId, address ierc721TokenAddress) {
         IERC721 nft = IERC721(ierc721TokenAddress);
-        if (msg.sender != nft.ownerOf(nftId)) {
+        if (msg.sender != nft.ownerOf(tokenId)) {
             revert NftMarketplace__Unauthorized();
         }
         _;
     }
 
-    modifier notListed(uint256 nftId, address ierc721TokenAddress) {
-        Listing memory listing = s_listings[ierc721TokenAddress][nftId];
+    modifier notListed(uint256 tokenId, address ierc721TokenAddress) {
+        Listing memory listing = s_listings[ierc721TokenAddress][tokenId];
         if (listing.price > 0) {
-            revert NftMarketplace__ItemAlreadyListed(nftId, ierc721TokenAddress);
+            revert NftMarketplace__ItemAlreadyListed(tokenId, ierc721TokenAddress);
         }
         _;
     }
 
-    modifier isListed(uint256 nftId, address ierc721TokenAddress) {
-        Listing memory listing = s_listings[ierc721TokenAddress][nftId];
+    modifier isListed(uint256 tokenId, address ierc721TokenAddress) {
+        Listing memory listing = s_listings[ierc721TokenAddress][tokenId];
         if (listing.price <= 0) {
-            revert NftMarketplace__ItemNotListed(nftId, ierc721TokenAddress);
+            revert NftMarketplace__ItemNotListed(tokenId, ierc721TokenAddress);
         }
         _;
     }
